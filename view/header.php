63f24124970b9f66ed5ca1ab5db3e787ce92c688
Mark Jaquith <m@rkj.me>
2011-08-20 01:38:42 -0400

    Move bootstrap to be in the css directory

--- view/header.php
+++ view/header.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <title><?php echo $title; ?></title>
-<link rel="stylesheet" href="bootstrap/bootstrap-1.0.0.min.css" />
+<link rel="stylesheet" href="css/bootstrap/bootstrap-1.0.0.min.css" />
 <link rel="stylesheet" href="css/main.css" />
 </head>
 <body>
