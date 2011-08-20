cb2d77475aee53f8bde0c1c2c307b288f0e98cea
Mark Jaquith <m@rkj.me>
2011-08-20 02:59:10 -0400

    Paste in the sample form

--- view/header.php
+++ view/header.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
+<meta charset="utf-8">
 <title><?php echo $title; ?></title>
-<link rel="stylesheet" href="css/bootstrap/bootstrap-1.0.0.min.css" />
-<link rel="stylesheet" href="css/main.css" />
+<link rel="stylesheet" href="css/bootstrap/bootstrap-1.0.0.min.css">
+<link rel="stylesheet" href="css/main.css">
 </head>
 <body>
