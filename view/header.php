aa52fd9ca46b655a24679ff3f78161127ced10a3
Mark Jaquith <m@rkj.me>
2011-08-20 01:31:46 -0400

    Fleshing out the basic app skeleton

---
+++ view/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title><?php echo $title; ?></title>
+<link rel="stylesheet" href="bootstrap/bootstrap-1.0.0.min.css" />
+<link rel="stylesheet" href="css/main.css" />
+</head>
+<body>
---
+++ view/index.php
@@ -0,0 +1,12 @@
+<?php include( 'header.php' ); ?>
+<div class="container">
+
+<div class="row">
+	<div id="left" class="span8 columns">Left Column</div>
+	<div id="right" class="span8 columns">Right Column</div>
+</div>
+<div class="row">
+	<div id="center" class="span8 columns offset4">Centered Column</div>
+
+</div>
+<?php include( 'footer.php' ); ?>
